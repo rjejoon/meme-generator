6f09d5fef89d3a06427943eb35597b14109fc4f0
Jejoon Ryu <rjejoon@gmail.com>
2022-10-31 22:19:28 -0600

    Get random image url on click

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,27 @@
 import './Meme.css';
+import memesData from '../memesData';
 
 
 export default function Meme() {
+  function getRandImg(e) {
+    e.preventDefault();
+    const rand_i = Math.floor(Math.random() * memesData.data.memes.length);
+    console.log(memesData.data.memes[rand_i].url);
+  }
+
   return (
     <main>
       <form action="" className="meme--form">
         <div className="meme--form-inputs-container">
-          <input type="text" value="Shut up"/>
-          <input type="text" value="and take my money"/>
+          <input type="text" placeholder="Top Text" />
+          <input type="text" placeholder="Bottom Text" />
         </div>
-        <button className="meme--form-btn" type="submit">Get a new meme image  🖼</button>
+        <button 
+          className="meme--form-btn" 
+          type="submit"
+          onClick={getRandImg}>
+          Get a new meme image  🖼
+        </button>
       </form>
     </main>
   );
